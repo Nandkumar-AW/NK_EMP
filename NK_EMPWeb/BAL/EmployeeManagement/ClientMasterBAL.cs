f0ce375b6b6143751c305f3c69d7d3403083f0c9
Nandkumar-AW <nandkumar@ascenwork.com>
2020-05-26 19:49:51 +0530

    Updated

---
+++ NK_EMPWeb/BAL/EmployeeManagement/ClientMasterBAL.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.SharePoint.Client;
+using Newtonsoft.Json.Linq;
+using NK_EMPWeb.DAL;
+using NK_EMPWeb.Models;
+using NK_EMPWeb.Models.EmployeeManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NK_EMPWeb.BAL.EmployeeManagement
+{
+    public class ClientMasterBAL
+    {
+        public List<Emp_ClientMasterDetailsModels> GetAllClient(ClientContext clientContext)
+        {
+            List<Emp_ClientMasterDetailsModels> emp_ClientMasterDetailsModels = new List<Emp_ClientMasterDetailsModels>();
+
+            JArray jArray = RESTGet(clientContext, null);
+
+            foreach (JObject j in jArray)
+            {
+                emp_ClientMasterDetailsModels.Add(new Emp_ClientMasterDetailsModels
+                {
+                    ID = Convert.ToInt32(j["ID"]),
+                    ClientName = j["ClientName"].ToString(),
+                    ClientAddress = j["ClientAddress"].ToString(),
+                    ClientContact=j["ClientContact"].ToString(),
+                    ClientMailID=j["ClientMailID"].ToString(),
+                    ClientDesignation=j["ClientDesignation"].ToString(),
+                    ClientGSTNO=j["ClientGSTNO"].ToString(),
+                    ClientPanCardNo=j["ClientPanCardNo"].ToString(),
+                    ClientState=j["ClientState"].ToString(),
+                    ClientCity=j["ClientCity"].ToString(),
+                    ClientRemark=j["ClientRemark"].ToString(),
+                    ClientCountry=j["ClientCountry"].ToString()
+                }); 
+            }
+
+
+            return emp_ClientMasterDetailsModels;
+
+        }
+        public List<Emp_ClientMasterDetailsModels> GetClientById(ClientContext clientContext,string id)
+        {
+            List<Emp_ClientMasterDetailsModels> emp_ClientMasterDetailsModels = new List<Emp_ClientMasterDetailsModels>();
+            
+            JArray jArray = RESTGetByID(clientContext, id);
+
+            foreach (JObject j in jArray)
+            {
+                emp_ClientMasterDetailsModels.Add(new Emp_ClientMasterDetailsModels
+                {
+                    ID = Convert.ToInt32(j["ID"]),
+                    ClientName = j["ClientName"].ToString(),
+                    ClientAddress = j["ClientAddress"].ToString(),
+                    ClientContact = j["ClientContact"].ToString(),
+                    ClientMailID = j["ClientMailID"].ToString(),
+                    ClientDesignation = j["ClientDesignation"].ToString(),
+                    ClientGSTNO = j["ClientGSTNO"].ToString(),
+                    ClientPanCardNo = j["ClientPanCardNo"].ToString(),
+                    ClientState = j["ClientState"].ToString(),
+                    ClientCity = j["ClientCity"].ToString(),
+                    ClientRemark = j["ClientRemark"].ToString(),
+                    ClientCountry = j["ClientCountry"].ToString()
+                });
+            }
+
+
+            return emp_ClientMasterDetailsModels;
+
+        }
+        public string SaveClient(ClientContext clientContext, string ItemData)
+        {
+
+            string response = RESTSave(clientContext, ItemData);
+
+            return response;
+        }
+
+        private JArray RESTGet(ClientContext clientContext, string filter)
+        {
+            RestService restService = new RestService();
+            JArray jArray = new JArray();
+            RESTOption rESTOption = new RESTOption();
+
+            rESTOption.filter = filter;
+            rESTOption.select = "ID,ClientName,ClientAddress,ClientContact,ClientMailID,ClientDesignation,ClientGSTNO,ClientPanCardNo,ClientState,ClientCity,ClientRemark,ClientCountry/CountryName";
+            rESTOption.expand = "ClientCountry";
+            rESTOption.top = "5000";
+            jArray = restService.GetAllItemFromList(clientContext, "Emp_ClientMasterDetails", rESTOption);
+
+            return jArray;
+        }
+
+        private JArray RESTGetByID(ClientContext clientContext, string ID)
+        {
+            RestService restService = new RestService();
+            JArray jArray = new JArray();
+            RESTOption rESTOption = new RESTOption();
+
+            rESTOption.select = "ID,FromDate,Todate,Reason,EmployeeName/Title,EmployeeName/ID,CurrentApprover/Title,CurrentApprover/ID,StatusMaster/StatusName,InternalStatus";
+            rESTOption.expand = "EmployeeName,CurrentApprover,StatusMaster";
+            rESTOption.top = "5000";
+
+
+            jArray = restService.GetItemByID(clientContext, "WorkFromHome", rESTOption, ID);
+
+            return jArray;
+        }
+
+
+        private string RESTSave(ClientContext clientContext, string ItemData)
+        {
+            RestService restService = new RestService();
+
+            return restService.SaveItem(clientContext, "Emp_ClientMasterDetails", ItemData);
+        }
+    }
+}
---
+++ NK_EMPWeb/Controllers/EmployeeManagement/ClientMasterController.cs
@@ -0,0 +1,77 @@
+﻿using NK_EMPWeb.BAL.EmployeeManagement;
+using NK_EMPWeb.Models.EmployeeManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NK_EMPWeb.Controllers.EmployeeManagement
+{
+    public class ClientMasterController : Controller
+    {
+        // GET: ClientMaster
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult ClientDashboard()
+        {
+            List<Emp_ClientMasterDetailsModels> emp_ClientMasterDetails = new List<Emp_ClientMasterDetailsModels>();
+            var spContext = SharePointContextProvider.Current.GetSharePointContext(HttpContext);
+            using (var clientContext = spContext.CreateUserClientContextForSPHost())
+            {
+                ClientMasterBAL clientMaster = new ClientMasterBAL();
+
+                emp_ClientMasterDetails = clientMaster.GetAllClient(clientContext);
+
+            }
+            ViewBag.ClientList = emp_ClientMasterDetails;
+            return View();
+        }
+
+        public ActionResult ClientDetails(int? id)
+        {
+            int ID = Convert.ToInt32(id);
+            List<Emp_ClientMasterDetailsModels> emp_ClientMasterDetails = new List<Emp_ClientMasterDetailsModels>();
+            var spContext = SharePointContextProvider.Current.GetSharePointContext(HttpContext);
+            using (var clientContext = spContext.CreateUserClientContextForSPHost())
+            {
+                ClientMasterBAL clientMaster = new ClientMasterBAL();
+
+                emp_ClientMasterDetails = clientMaster.GetClientById(clientContext, id.ToString());
+
+            }
+            return View(emp_ClientMasterDetails);
+        }
+
+        [HttpGet]
+        public ActionResult CreateNewClient()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateNewClient(Emp_ClientMasterDetailsModels _ClientMasterDetailsModels)
+        {
+            var spContext = SharePointContextProvider.Current.GetSharePointContext(HttpContext);
+            using (var clientContext = spContext.CreateUserClientContextForSPHost())
+            {
+                ClientMasterBAL clientMaster = new ClientMasterBAL();
+                string items = "'ClientName':" + _ClientMasterDetailsModels.ClientName;
+                       //items += ",'ClientAddress':" + _ClientMasterDetailsModels.ClientAddress;
+                       //items += ",'ClientContact':" + _ClientMasterDetailsModels.ClientContact;
+                       //items += ",'ClientMailID':" + _ClientMasterDetailsModels.ClientMailID;
+                       //items += ",'ClientDesignation':" + _ClientMasterDetailsModels.ClientDesignation;
+                       //items += ",'ClientGSTNO':" + _ClientMasterDetailsModels.ClientGSTNO;
+                       //items += ",'ClientPanCardNo':" + _ClientMasterDetailsModels.ClientPanCardNo;
+                       //items += ",'ClientState':" + _ClientMasterDetailsModels.ClientState;
+                       //items += ",'ClientCity':" + _ClientMasterDetailsModels.ClientCity;
+                       //items += ",'ClientRemark':" + _ClientMasterDetailsModels.ClientRemark;
+                //items += ",'ClientCountry':" + _ClientMasterDetailsModels.ClientCountry;
+                clientMaster.SaveClient(clientContext, items);
+            }
+            return View();
+        }
+    }
+}
